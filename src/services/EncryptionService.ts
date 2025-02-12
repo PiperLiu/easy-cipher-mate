43a7b9127254f90d296797b7240cc42e37005a3b
LHJ Piper <2940596912@qq.com>
2025-02-12 23:12:45 +0800

    Add utils function

--- src/services/EncryptionService.ts
+++ src/services/EncryptionService.ts
@@ -1,7 +1,8 @@
 import { IEncryptionAlgorithm, EncryptionResult, IEncryptionAlgorithmConfig } from '../encryption/IEncryptionAlgorithm';
+import { readFileSync, writeFileSync } from 'fs';
 
 export class EncryptionService {
-    constructor(private algorithm: IEncryptionAlgorithm, private algorithmConfig: IEncryptionAlgorithmConfig) {}
+    constructor(private algorithm: IEncryptionAlgorithm, private algorithmConfig: IEncryptionAlgorithmConfig) { }
 
     async encryptText(plaintext: string): Promise<EncryptionResult> {
         return this.algorithm.encryptText(plaintext, this.algorithmConfig);
@@ -17,5 +18,27 @@ export class EncryptionService {
 
     async decryptFile(encryptedBuffer: ArrayBuffer): Promise<ArrayBuffer> {
         return this.algorithm.decryptFile(encryptedBuffer, this.algorithmConfig);
+    }
+
+    async encryptFileByName(fileName: string): Promise<EncryptionResult> {
+        const fileBuffer = readFileSync(fileName);
+        const arrayBuffer = fileBuffer.buffer.slice(fileBuffer.byteOffset, fileBuffer.byteOffset + fileBuffer.byteLength);
+        return this.encryptFile(arrayBuffer);
+    }
+
+    async decryptFileByName(encryptedResult: EncryptionResult, fileName: string): Promise<void> {
+        const decryptedBuffer = await this.decryptFile(encryptedResult.data);
+        const uint8Array = new Uint8Array(decryptedBuffer);
+        writeFileSync(fileName, uint8Array);
+    }
+
+    arrayBufferToString(buffer: ArrayBuffer): string {
+        const decoder = new TextDecoder('utf-8');
+        return decoder.decode(buffer);
+    }
+
+    stringToArrayBuffer(str: string): ArrayBuffer {
+        const encoder = new TextEncoder();
+        return encoder.encode(str).buffer;
     }
 }
